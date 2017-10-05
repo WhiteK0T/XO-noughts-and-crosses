e36de25f164e9fa78bcab88669dec9531a7e4699
Aleksey Savenkov <alex.savenkov@inbox.ru>
2017-10-05 17:18:31 +0400

    Field - test for the Point added

--- src/ru/whitek0t/xo/model/Field.java
+++ src/ru/whitek0t/xo/model/Field.java
@@ -1,10 +1,17 @@
 package ru.whitek0t.xo.model;
 
+import ru.whitek0t.xo.model.exceptions.AlreadyOccupiedException;
+import ru.whitek0t.xo.model.exceptions.InvalidPointException;
+
 import java.awt.*;
 
 public class Field {
 
     private static final int FIELD_SIZE = 3;
+
+    private static final int MIN_COORDINATE = 0;
+
+    private static final int MAX_COORDINATE = FIELD_SIZE;
 
     private final Figure[][] field = new Figure[FIELD_SIZE][FIELD_SIZE];
 
@@ -12,11 +19,28 @@ public class Field {
         return FIELD_SIZE;
     }
 
-    public Figure getFigure(final Point point) {
+    public Figure getFigure(final Point point) throws InvalidPointException {
+        if (!checkPoint(point)) {
+            throw new InvalidPointException();
+        }
         return field[point.x][point.y];
     }
 
-    public void setFigure(final Point point, final Figure figure) {
+    public void setFigure(final Point point, final Figure figure) throws InvalidPointException, AlreadyOccupiedException {
+        if (!checkPoint(point)) {
+            throw new InvalidPointException();
+        }
+        if (field[point.x][point.y] != null) {
+            throw new AlreadyOccupiedException();
+        }
         field[point.x][point.y] = figure;
+    }
+
+    private boolean checkPoint(final Point point) {
+        return cheakCoordinate(point.x) && cheakCoordinate(point.y);
+    }
+
+    private boolean cheakCoordinate(final int coordinate) {
+        return coordinate >= MIN_COORDINATE && coordinate < MAX_COORDINATE;
     }
 }
