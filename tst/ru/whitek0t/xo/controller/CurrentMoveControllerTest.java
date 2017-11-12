691d9288742cf1476f2c1ed9990a272431bfb8d3
Aleksey Savenkov <alex.savenkov@inbox.ru>
2017-11-12 19:04:07 +0400

    CurrentMoveControllerTest Refactoring the field to the size of any value

--- tst/ru/whitek0t/xo/controller/CurrentMoveControllerTest.java
+++ tst/ru/whitek0t/xo/controller/CurrentMoveControllerTest.java
@@ -13,7 +13,7 @@ public class CurrentMoveControllerTest {
     public void testСurrentMoveWhenNextMoveIsO() throws Exception {
         final CurrentMoveController currentMoveController = new CurrentMoveController();
         for (int i = 0; i < 3; i++) {
-            final Field field = new Field(3);
+            final Field field = new Field(3, 4, 3);
             field.setFigure(new Point(i, 0), Figure.X);
             field.setFigure(new Point(i, 1), Figure.O);
             field.setFigure(new Point(i, 2), Figure.X);
@@ -24,7 +24,7 @@ public class CurrentMoveControllerTest {
     public void testСurrentMoveWhenNextMoveIsX() throws Exception {
         final CurrentMoveController currentMoveController = new CurrentMoveController();
         for (int i = 0; i < 3; i++) {
-            final Field field = new Field(3);
+            final Field field = new Field(3, 4, 3);
             field.setFigure(new Point(i, 1), Figure.O);
             field.setFigure(new Point(i, 2), Figure.X);
             assertEquals(Figure.X, currentMoveController.currentMove(field));
@@ -33,16 +33,19 @@ public class CurrentMoveControllerTest {
     @Test
     public void testСurrentMoveWhenNoNextMove() throws Exception {
         final CurrentMoveController currentMoveController = new CurrentMoveController();
-        final Field field = new Field(3);
+        final Field field = new Field(3, 4, 3);
         field.setFigure(new Point(0, 0), Figure.O);
         field.setFigure(new Point(0, 1), Figure.X);
         field.setFigure(new Point(0, 2), Figure.O);
+        field.setFigure(new Point(0, 3), Figure.X);
         field.setFigure(new Point(1, 0), Figure.O);
         field.setFigure(new Point(1, 1), Figure.X);
         field.setFigure(new Point(1, 2), Figure.O);
+        field.setFigure(new Point(1, 3), Figure.X);
         field.setFigure(new Point(2, 0), Figure.O);
         field.setFigure(new Point(2, 1), Figure.X);
         field.setFigure(new Point(2, 2), Figure.O);
+        field.setFigure(new Point(2, 3), Figure.X);
         assertNull(currentMoveController.currentMove(field));
     }
 }
