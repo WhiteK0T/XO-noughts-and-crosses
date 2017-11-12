e76758727388f9a4af5f7849a72cc9c2796e0d75
Aleksey Savenkov <alex.savenkov@inbox.ru>
2017-11-12 14:44:00 +0400

    CurrentMoveController Refactoring the field to the size of any value

--- src/ru/whitek0t/xo/controller/CurrentMoveController.java
+++ src/ru/whitek0t/xo/controller/CurrentMoveController.java
@@ -3,16 +3,17 @@ package ru.whitek0t.xo.controller;
 import ru.whitek0t.xo.model.Field;
 import ru.whitek0t.xo.model.Figure;
 import ru.whitek0t.xo.model.exceptions.InvalidPointException;
+import ru.whitek0t.xo.model.exceptions.XOCriticalException;
 
 import java.awt.*;
 
 public class CurrentMoveController {
     public Figure currentMove(final Field field) {
         int countFigure = 0;
-        for (int y = 0; y < field.getSize(); y++) {
+        for (int y = 0; y < field.getSizeY(); y++) {
             countFigure += countFigureInTheRow(field, y);
         }
-        if (countFigure == field.getSize() * field.getSize())
+        if (countFigure == field.getSizeX() * field.getSizeY())
             return null;
         if (countFigure % 2 == 0)
             return Figure.X;
@@ -22,12 +23,13 @@ public class CurrentMoveController {
 
     private int countFigureInTheRow (final Field field, final int row) {
         int countFigure = 0;
-        for (int x = 0; x < field.getSize(); x++) {
+        for (int x = 0; x < field.getSizeX(); x++) {
             try {
                 if (field.getFigure(new Point(x, row)) != null)
                     countFigure++;
             } catch (InvalidPointException e) {
                 e.printStackTrace();
+                throw new XOCriticalException(e);
             }
         }
         return countFigure;
